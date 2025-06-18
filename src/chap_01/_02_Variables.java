898b9b31410654aba690ccc5fb303adbf067359e
BBDUYU <tmdwhddh@gmail.com>
2025-06-18 19:34:51 +0900

    변수_1

---
+++ src/chap_01/_02_Variables.java
@@ -0,0 +1,10 @@
+package chap_01;
+
+public class _02_Variables {
+    public static void main(String[] args) {
+        String name;
+        name="홍길동";
+        System.out.println(name+"님, 배송이 시작됩니다. 15시에 방문 예정입니다");
+        System.out.println(name+"님, 배송이 완료되었습니다");
+    }
+}
