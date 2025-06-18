93e03809376aef8b7882224a038cad796ae957ce
BBDUYU <tmdwhddh@gmail.com>
2025-06-18 19:31:04 +0900

    자료형

---
+++ src/chap_01/_01_DataTypes.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _01_DataTypes {
+    public static void main(String[] args) { //단축키 psvm or main
+        System.out.println("hello world"); // 단축키 sout
+        System.out.println("안녕하세요");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123+345);
+    }
+}
